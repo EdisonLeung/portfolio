10eed1f75762e676b08e83ee8a5aaaa3cb2983f8
Edison Leung <edleung@cs.washington.edu>
2023-01-03 11:46:13 -0800

    Updates

---
+++ precache-manifest.897f09545348c28f98ed311db2f9b673.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d72d270e0c20813109c",
+    "url": "/portfolio/static/css/main.979c7acd.chunk.css"
+  },
+  {
+    "revision": "5d72d270e0c20813109c",
+    "url": "/portfolio/static/js/main.5d72d270.chunk.js"
+  },
+  {
+    "revision": "0bb67be269791cb99fe0",
+    "url": "/portfolio/static/js/1.0bb67be2.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "22b1e26f9f7a5972e2007934c78f5995",
+    "url": "/portfolio/index.html"
+  }
+];

1120eaa306422f4b629b649d08b07b006cac00cd
Edison Leung <edleung@cs.washington.edu>
2023-08-25 23:20:34 -0700

    Updates

---
+++ precache-manifest.c6ee88ae24101c1e42dacd28090f020b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a444463b8846f67ce66e",
+    "url": "/portfolio/static/css/main.7f445aee.chunk.css"
+  },
+  {
+    "revision": "a444463b8846f67ce66e",
+    "url": "/portfolio/static/js/main.a444463b.chunk.js"
+  },
+  {
+    "revision": "ce27218fd1e95b9a867c",
+    "url": "/portfolio/static/css/1.cc1e6a83.chunk.css"
+  },
+  {
+    "revision": "ce27218fd1e95b9a867c",
+    "url": "/portfolio/static/js/1.ce27218f.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "ef0414840719ac771b32a72aa13c4613",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e98ee4923d45f3d5b7288ebcf3e2035d.js"
+  "/portfolio/precache-manifest.c6ee88ae24101c1e42dacd28090f020b.js"
 );
 
 workbox.clientsClaim();

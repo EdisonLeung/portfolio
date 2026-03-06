94f400825fc417e153bd4bd3b6bfa362f2a51ee3
Edison Leung <edleung03@gmail.com>
2026-03-05 20:28:03 -0800

    Updates

---
+++ precache-manifest.2e8545d49c2e37b0253a12e74e6a361c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b26bcb7fde427bd4cfa0",
+    "url": "/portfolio/static/css/main.1bea7e6d.chunk.css"
+  },
+  {
+    "revision": "b26bcb7fde427bd4cfa0",
+    "url": "/portfolio/static/js/main.b26bcb7f.chunk.js"
+  },
+  {
+    "revision": "dd1c956cc4e7fa07b2c9",
+    "url": "/portfolio/static/css/1.c9307a70.chunk.css"
+  },
+  {
+    "revision": "dd1c956cc4e7fa07b2c9",
+    "url": "/portfolio/static/js/1.dd1c956c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "48794c5a6840a8fe7a09c9fdfa84ca0c",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.56e3687a5d00623a0681b1ac3f1b9949.js"
+  "/portfolio/precache-manifest.2e8545d49c2e37b0253a12e74e6a361c.js"
 );
 
 workbox.clientsClaim();
